cfdb30658c08f652bbd6f93a4e3b3a7652de2fea
vedao <vsijercic@gmail.com>
2017-06-29 13:43:17 +0200

    Modal functionality and site too

--- app.js
+++ app.js
@@ -3,11 +3,39 @@
 //---------------------------------------------
 
 $(document).ready(function() {
+
+    //---------------------------------------------
+    //   ADD LINK
+    //---------------------------------------------
+
     $('.js-like').on('click', function(event) {
         event.preventDefault();
 
         $(this).text('Liked!')
             .closest('.news-item')
             .addClass('is-liked');
-    })
+    });
+
+    $('.js-add-link').on('click', function(event) {
+        event.preventDefault();
+
+        $('.js-form').toggleClass('is-visible');
+    });
+
+    //---------------------------------------------
+    //   ADD LINK
+    //---------------------------------------------
+
+    $('.js-show-modal').on('click', function(event) {
+        event.preventDefault();
+
+        $('.js-modal').addClass('is-visible');
+        $('.js-modal-overlay').addClass('is-visible');
+    });
+
+    $('.js-modal-overlay').on('click', function(event) {
+        $('.js-modal').removeClass('is-visible');
+        $('.js-modal-overlay').removeClass('is-visible');
+
+    });
 });
